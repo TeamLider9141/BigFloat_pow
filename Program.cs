364c4c2770f8eb1c0a3784b094e8321107dc2bc4
TeamLider9141 <juraqulovortiq2002@gmail.com>
2022-03-12 12:42:00 +0500

    BigFloat_Pow

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using static System.Math;
+
+using System.Numerics;
+
+namespace acmp1
+{
+    public class Program
+    {
+        static void Main(string[] args)   // pow(n,a) , pow(a,b)  BigFloat, BigDouble, BigDecimal,  BigInteger   a Degree b 
+        {
+
+            BigInteger s=1;   
+
+            BigInteger n = BigInteger.Parse(Console.ReadLine());    // sonni kiritamiz :   input = a 
+            BigInteger a = BigInteger.Parse(Console.ReadLine());    // darajani kiritamiz:   input = b 
+
+            for (int i=1; i<=n; i++)  // sikl orqali qayta-qayta darajaga ko'taramiz  
+            {
+                 s = s * a;
+            }
+            Console.WriteLine(s);  //    output:  s=  pow(n,a)  
+
+        }
+
+    }
+}
